f3e4355ede026499fa2893cf0a8ceec5db4999e5
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2020-03-26 17:30:02 -0400

    Shortened movie descriptions

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -4,19 +4,12 @@ import { useState } from 'react';
 const SideMenu = (props) => {
     return(
         <div>
-            {/* clickHandler={() => { console.log("Check out the count in the console: " + {count});}} */}
-            <button onClick={props.clickHandler}>Click ME!</button>
-
             {/* appName={"Movie DB"} */}
             <h1 className="my-4">{props.appName}</h1>
             <div className="list-group">
                 <a href="#" className="list-group-item">Category 1</a>
                 <a href="#" className="list-group-item">Category 2</a>
                 <a href="#" className="list-group-item">Category 3</a>
-            </div>
-            <div>
-                {/* <SideMenu whatEverIWant={count}/> */}
-                <h1>{props.whatEverIWant}</h1>
             </div>
         </div>
     );
