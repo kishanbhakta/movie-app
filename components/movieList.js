9c46b44b13e599bfecfbb2866759ed0d33625fd2
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2020-03-26 13:21:39 -0400

    MovieList ==> SideBar  [Functions, State, Props, useState: 'import', this]

--- components/movieList.js
+++ components/movieList.js
@@ -4,9 +4,64 @@ import  React from 'react';
 * React.Fragment creates a node not registered by the DOM but can be keyed
 * */
 class MovieList extends React.Component {
+   constructor(props) {
+       super(props);
+       this.state = {
+           count: 0
+       };
+       // .bind(this) binds the this context to the function !written as ` functionName => () {}`
+       this.increment = this.increment.bind(this);
+   };
+
+    increment (){
+        // Utilize State inside Component
+        const { count } = this.state;
+
+        // Access Debugger and remember to `//` when !USING
+        //debugger
+
+        // Using React.Component functions to access and mutate the state property
+        this.setState({
+            count: count + 1,
+        })
+    };
+
+    /*
+    *
+    * NOTICE THE DIFFERENCE IN HOW THE INCREMENT AND DECREMENT FUNCTIONS ARE DECLARED
+    *   - CHECKOUT THE CONSTRUCTOR
+    *   - HOW IS IT UTILIZED?
+    *
+    * */
+    decrement = () => {
+        const { count } = this.state;
+
+        this.setState({
+            count: count - 1,
+        })
+    };
+
+
     render() {
+        // Access Debugger and remember to `//` when !USING
+        // debugger
+
         return (
             <React.Fragment>
+
+                <div>
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.increment}
+                    >Increment Number</button>
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.decrement}
+                    >Decrement Number</button>
+                    <h1>{this.state.count}</h1>
+                </div>
+
+
                 <div className="col-lg-4 col-md-6 mb-4">
                     <div className="card h-100">
                         <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
@@ -110,3 +165,17 @@ class MovieList extends React.Component {
 };
 
 export default MovieList;
+
+/*
+***Summary***
+`State` is referred to the local state of the component which cannot be accessed and modified outside of the component and only can be used & modified inside the component.
+
+`Props` make components reusable by giving components the ability to receive data from the parent component in the form of props.
+
+	`this.prop.propertyName` to access the attributes
+
+	pass as argument `(propertyName)` ========> utilize in code `${propertyName}`
+
+***Common Mistakes***
+!USING arrow functions to continue to bind the this context to the function.
+*/
