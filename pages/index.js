45c4765754e5ca6a483db0184db8c56880e87dff
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2020-03-27 15:44:13 -0400

    class home extends React.Component

--- pages/index.js
+++ pages/index.js
@@ -9,76 +9,81 @@ import Footer from "../components/footer";
 // Import getter methods from ../actions
 import { getMovies } from "../actions";
 
-const Home = () => {
-    // IMPORTANT!!!
-    // Getter and Setters to transfer data from async call
-    const [movies, setMovies] = useState([]);
-    const [count, setCount] = useState(0);
+class Home extends React.Component {
     /*
-    * The function passed to useEffect will run after the render is committed to the screen.
+    * componentDidMount() is invoked immediately after a component is mounted (inserted into the tree).
     *
-    * - By default `effects` run after every completed render, but you can choose to fire them
-    *   only when certain values have changed
+    * Instantiate Network Request from remote endpoint(s) here.
     *
-    * -
-    */
-    useEffect(() => {
-        const fetchData = async () => {
-            const resMovies = await getMovies();
-            setMovies(resMovies);
-        };
+    * You may call setState() immediately in componentDidMount().
+    * - It will happen before the browser updates the screen
+    * - User will not see the intermediate states and can cause performance issues
+    * - Assign initial state in the constructor instead
+    *
+    * */
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        }
+    }
+    componentDidMount() {
+        getMovies().then((movies) => {
+            this.setState({movies})
+        })
+    }
 
-        fetchData();
+    render() {
+        const { movies } = this.state;
+        return (
+            <div>
+                <Head>
+                    <title>Home</title>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
+                </Head>
+                {/*NAVBAR*/}
+                <NavBar />
 
-    }, [count]); // add more arg to useEffect()
-
-    return(
-        <div>
-            <Head>
-                <title>Home</title>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-            </Head>
-            {/*NAVBAR*/}
-            <NavBar />
-
-            {/*BODY*/}
-            <div className="home-page">
-                <div className="container">
-                    <button onClick={() => setCount(count + 1)}>Click Me!</button>
-                    <div className="row">
-                        <div className="col-lg-3">
-                            {/*Utilize props by setting someName={propName}*/}
-                            <SideMenu
-                                appName={"Movie DB"}/>
-                        </div>
-
-                        <div className="col-lg-9">
-                            <Carousel />
-                            <div className="row">
-                                <MovieList movies={movies}/>
+                {/*BODY*/}
+                <div className="home-page">
+                    <div className="container">
+                        <button onClick={() => setCount(count + 1)}>Click Me!</button>
+                        <div className="row">
+                            <div className="col-lg-3">
+                                {/*Utilize props by setting someName={propName}*/}
+                                <SideMenu
+                                    appName={"Movie DB"}/>
                             </div>
-                        </div>
 
+                            <div className="col-lg-9">
+                                <Carousel />
+                                <div className="row">
+                                    <MovieList movies={movies}/>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
+
+                {/*STYLES*/}
+                <style jsx>{`
+                    .home-page {
+                        padding-top: 80px;
+                    }
+                `}
+                </style>
+
+                {/*FOOTER*/}
+                <Footer />
+
             </div>
+        );
+    }
 
-            {/*STYLES*/}
-            <style jsx>{`
-                .home-page {
-                    padding-top: 80px;
-                }
-            `}
-            </style>
-
-            {/*FOOTER*/}
-            <Footer />
-
-        </div>
-    );
 };
 
 export default Home;
