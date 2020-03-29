7ba8b11c461bec9fe612d91e20215cf768ebf113
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2020-03-29 19:14:51 -0400

    Created App wrapper for all components to render to Need to getInitilProps to App component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import Head from 'next/head';
-import NavBar from "../components/navbar";
 import SideMenu from "../components/sideMenu";
 import Carousel from "../components/carousel";
 import MovieList from "../components/movieList";
-import Footer from "../components/footer";
 
 // Import getter methods from ../actions
 import { getMovies } from "../actions";
@@ -12,16 +9,6 @@ import { getMovies } from "../actions";
 const Home = (props) => {
     return (
         <div>
-            <Head>
-                <title>Home</title>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-            </Head>
-            {/*NAVBAR*/}
-            <NavBar />
-
             {/*BODY*/}
             <div className="home-page">
                 <div className="container">
@@ -40,25 +27,13 @@ const Home = (props) => {
                                 {/*        {errorMessage}*/}
                                 {/*    </div>*/}
                                 {/*}*/}
-                                <MovieList movies={props.movies}/>
+                                <MovieList movies={props.movies || []}/>
                             </div>
                         </div>
 
                     </div>
                 </div>
             </div>
-
-            {/*STYLES*/}
-            <style jsx>{`
-                    .home-page {
-                        padding-top: 80px;
-                    }
-                `}
-            </style>
-
-            {/*FOOTER*/}
-            <Footer />
-
         </div>
     );
 };
