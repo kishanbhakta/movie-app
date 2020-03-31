b49c5790bc3282ccb1e8cc928031b9e7dc5d7e7d
Kishan Bhakta <kishan.d.bhakta@gmail.com>
2020-03-30 23:19:16 -0400

    edited custom server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+// Import nextjs and express frameworks
+const next = require('next');
 const express = require('express');
 
+// check if in dev or prod env localhost or cloud servers
 const dev = process.env.NODE_ENV !== 'production'
+// create express app
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
 
+  // create express server  
   const server = express();
+  // Handle all request coming to the server
   server.get('*', (req, res) => {
+    // Parse req and serve component page
+    // next.js is handling this and providing pages where we are navigating to
     return handle(req, res)
+    
   })
 
+  // get port from env variable or assign port at 3000
   const PORT = process.env.PORT || 3000;
 
-  server.use(handle).listen(PORT, (err) => {
+  server.listen(PORT, (err) => {
     if (err) throw err
     console.log('> Ready on port ' + PORT)
   })
